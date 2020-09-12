fd70cf9352c7c8ab571515d837707d410458fa05
descifrado <anujmodi2011@gmail.com>
2020-09-12 20:57:23 +0530

    Initial Commit

---
+++ src/stopwatch/Main.java
@@ -0,0 +1,12 @@
+package stopwatch;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args){
+        Stopwatch mysw =new Stopwatch();
+        mysw.setVisible(true);
+    }
+}
---
+++ src/stopwatch/Timer.java
@@ -0,0 +1,46 @@
+
+package stopwatch;
+
+/**
+ *
+ * @author HP
+ */
+public class Timer implements Runnable{
+    Stopwatch mysw;
+    public volatile boolean status = true;
+    public Timer(Stopwatch sw){
+        mysw =sw;
+    }
+
+    @Override
+    public void run() {
+        int hr=0;
+        int min=0;
+        int sec=0;
+        
+        try{
+            while(status){
+                Thread.sleep(1000);
+                sec++;
+                if(sec==60){
+                    min++;
+                    sec=0;
+                }
+                if(min==60){
+                    hr++;
+                    min=0;
+                }
+                // UI display code.
+                mysw.setTimer(hr, min, sec);
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void stop(){
+        status=false;
+    }
+    
+}
